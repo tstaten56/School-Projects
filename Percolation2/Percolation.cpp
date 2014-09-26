a11a8cadaa6ad3d572aa3284e9566579eaac9f36
tstaten56 <tstaten56@gmail.com>
2014-09-26 16:07:26 -0400

    Adding the files again. TAKE 5!

---
+++ Percolation2/Percolation.cpp
@@ -0,0 +1,18 @@
+#include "Percolation.h"
+
+
+Percolation::Percolation()
+{
+}
+
+//Create N x N grid
+Percolation::Percolation(int n)
+{
+	int *arr; //Here comment
+}
+
+
+
+Percolation::~Percolation()
+{
+}
