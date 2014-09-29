ec3757df0bcc8ad83ac086a42f750baf419ddfab
tstaten56 <tstaten56@gmail.com>
2014-09-29 14:57:23 -0400

    Added most functions
    Only a couple are implemented so far!

--- Percolation2/Percolation.h
+++ Percolation2/Percolation.h
@@ -1,15 +1,20 @@
 #pragma once
+#include <vector>
+using namespace std;
 class Percolation
 {
 private:
-
+	vector< vector<bool> > grid;
+	int size = 0;
 public:
-	Percolation(); 
+	Percolation();
 	Percolation(int n);			  // create N-by-N grid, with all sites blocked
 	void open(int i, int j);      // open site (row i, col j) if it is not already
 	bool isOpen(int i, int j);    // is site (row i, col j) open?
 	bool isFull(int i, int j);    // is site (row i, col j) full?
 	bool percolates();            // does the system percolate?
+	void unblock(double p);		  //Unblock cells with probability p
+	void outputGrid();
 
 	~Percolation();
 };
