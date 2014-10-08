c70fbbb02c665820820b313347da504653844aba
tstaten56 <tstaten56@gmail.com>
2014-10-08 14:14:42 -0400

    Unblock and Open written
    Tested and wrote unblock {} and bool open {}

--- Percolation2/Percolation.cpp
+++ Percolation2/Percolation.cpp
@@ -1,5 +1,6 @@
 #include "Percolation.h"
 #include <iostream>
+#include <time.h>
 
 Percolation::Percolation()
 {
@@ -25,12 +26,19 @@ Percolation::~Percolation()
 
 void Percolation::open(int i, int j)
 {
-
+	grid[i][j] = true;
 }
 
 bool Percolation::isOpen(int i, int j)
 {
-	return true;
+	if (grid[i][j] == true)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 bool Percolation::isFull(int i, int j)
@@ -45,7 +53,19 @@ bool Percolation::percolates()
 
 void Percolation::unblock(double p)
 {
-
+	//Function to unblock the cells based on factor of  p
+	srand(time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int rn = rand() % 100;
+			if (rn < (p*100))
+			{
+				open(i, j);
+			}
+		}
+	}
 }
 
 void Percolation::outputGrid()
@@ -76,11 +96,52 @@ void InitializeTests()
 
 }
 
+void OpenTests()
+{ //test isOpen and Open functions
+	Percolation ex1(2);
+	ex1.open(0, 0);
+	cout << "opened 0, 0 \n";
+	ex1.outputGrid();
+	cout << "isOpen should be true: " << ex1.isOpen(0, 0) << endl;
+	cout << "isOpen should be false: " << ex1.isOpen(1, 0) << endl;
+	cout << "opened 0, 1 \n";
+	ex1.open(0, 1);
+	ex1.outputGrid();
+	ex1.open(1, 0);
+	cout << "opened 1, 0 \n";
+	ex1.outputGrid();
+	ex1.open(1, 1);
+	cout << "opened 1, 1 \n";
+	ex1.outputGrid();
 
+}
+
+void UnblockTests()
+{
+	Percolation ex1(2);
+	ex1.unblock(.5);
+	cout << "p = .5 on a size 2 grid: \n";
+	ex1.outputGrid();
+	Percolation ex2(10);
+	ex2.unblock(.3);
+	cout << "p = .3 on a size 10 grid: \n";
+	ex2.outputGrid();
+	Percolation ex3(10);
+	ex3.unblock(.5);
+	cout << "p = .5 on a size 10 grid: \n";
+	ex3.outputGrid();
+	Percolation ex4(10);
+	ex4.unblock(.59);
+	cout << "p = .59 on a size 10 grid: \n";
+	ex4.outputGrid();
+}
 
 void PercolationTests()
 {
 	//InitializeTests();
+	//OpenTests(); 
+	//UnblockTests();
+
 }
 
 int main()
