e4ce0db79d58f1492c90fe70074f1589570d3029
tstaten56 <tstaten56@gmail.com>
2014-10-10 14:09:09 -0400

    Added getParent(int i, int j)
    Used for finding the parent of a node, and updating that to be the shortest path within the trees. So short height tree

--- Percolation2/Percolation.h
+++ Percolation2/Percolation.h
@@ -5,17 +5,19 @@ using namespace std;
 class Percolation
 {
 private:
-	vector< vector<bool> > grid;
+	vector< vector<bool> > grid; //building the open/closed graph
+	vector< vector<int> > graph; //building trees
 	int size = 0;
 public:
 	Percolation();
 	Percolation(int n);			  // create N-by-N grid, with all sites blocked
-	void open(int i, int j);      // open site (row i, col j) if it is not already
+	void open(int i, int j);      // open site (row i, col j) if it is not already add to the path array
 	bool isOpen(int i, int j);    // is site (row i, col j) open?
 	bool isFull(int i, int j);    // is site (row i, col j) full?
 	bool percolates();            // does the system percolate?
 	void unblock(double p);		  //Unblock cells with probability p
 	void outputGrid();
+	int getParent(int i, int j);
 
 	~Percolation();
 };
