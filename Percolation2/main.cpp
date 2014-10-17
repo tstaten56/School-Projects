018306a61c64a9150b84bd7b3609a6d1ec5242dd
tstaten56 <tstaten56@gmail.com>
2014-10-17 17:15:19 -0400

    Added file reading
    File reading added

--- Percolation2/main.cpp
+++ Percolation2/main.cpp
@@ -1,45 +1,43 @@
-//#include <string>
-//#include "Percolation.h"
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main(int argc, char *argv[])
-//{
-//	/*
-//		yourProgram.exe p n s // p - percolation probability; n - # of runs; s – board size; 
-//		// your program should output percolation rate
-//		yourProgram.exe fname.txt // the game board setting is saved in the file named fname.txt 
-//		//click here for a sample input. 1 stands for open site; 0 for closed site.
-//		// your program should output the number of clusters on the board
-//	*/
-//	string temp;
-//	//temp = argv[0];
-//	temp = argv[1];
-//	temp = argv[2];
-//	temp = argv[3];
-//	//temp = argv[4];
-//	if ((argc == 1) || (argc == 3) || (argc > 4))
-//	{
-//		cout << "Wrong input format!" << endl;
-//		exit(1);
-//	}
-//	if (argc == 4)
-//	{
-//		double p;
-//		int s, n;
-//		p = atol(argv[1]); //percolation probability
-//		n = atoi(argv[2]); //n # of runs
-//		s = atoi(argv[3]); // s - board size
-//
-//
-//		Percolation grid(s);
-//		grid.unblock(p);
-//	}
-//	if (argc == 2)  //yourprogram.exe fname.txt   //game board setting is saved in the file named fname.txt   
-//	{				//Program should output the number of clusters on the board
-//
-//	}
-//	return 0;
-//}
-//
+#include <string>
+#include "Percolation.h"
+#include <iostream>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
+	/*
+		yourProgram.exe p n s // p - percolation probability; n - # of runs; s – board size; 
+		// your program should output percolation rate
+		yourProgram.exe fname.txt // the game board setting is saved in the file named fname.txt 
+		//click here for a sample input. 1 stands for open site; 0 for closed site.
+		// your program should output the number of clusters on the board
+	*/
+	if ((argc == 1) || (argc == 3) || (argc > 4))
+	{
+		cout << "Wrong input format!" << endl;
+		exit(1);
+	}
+	if (argc == 4)
+	{
+		double p;
+		int s, n;
+		p = atof(argv[1]); //percolation probability
+		n = atoi(argv[2]); //n # of runs
+		s = atoi(argv[3]); // s - board size
+		Percolation g;
+		cout << "p: " << p << ", n: " << n << ", s: " << s << endl;
+		cout << "Percolation rate: " << g.percolationRate(p, n, s) << endl;
+	}
+	if (argc == 2)  //yourprogram.exe fname.txt   //game board setting is saved in the file named fname.txt   
+	{				//Program should output the number of clusters on the board
+		string fileName = argv[1];
+	    Percolation g;
+		g.readFile(fileName);
+	}
+	return 0;
+}
+
