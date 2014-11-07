bcd07a4bfa8c6f469c0da974d8b131dd7b2c341b
tstaten56 <tstaten56@gmail.com>
2014-11-07 16:35:52 -0500

    Created main.cpp
    Here is main.cpp

---
+++ SeamCarving/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "SeamCarving.h"
+using namespace std;
+
+//int main (int argc, char *argv[])
+//{
+//	if (argc == 4)
+//	{
+//		string file;
+//		int vert, horz;
+//		file = argv[1];
+//		horz = atoi(argv[2]);
+//		vert = atoi(argv[3]);
+//		SeamCarving ex;
+//		ex.readFile(file);
+//	}
+//	else
+//	{
+//		cout << "Wrong input format!" << endl;
+//		return 1;
+//	}
+//
+//	return 0;
+//}
