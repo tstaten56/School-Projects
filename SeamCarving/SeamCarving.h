28d4bf1b0483177dce9f00bfa816bf219ed9e015
tstaten56 <tstaten56@gmail.com>
2014-11-11 17:19:41 -0500

    Removed unused functions

--- SeamCarving/SeamCarving.h
+++ SeamCarving/SeamCarving.h
@@ -11,10 +11,10 @@ private:
 	string type;
 	string comment;
 	int max;
-	int **picture;
-	int *original;
-	vector< vector<int> > pic;
-	vector< vector<int> > energy;
+	int **image;
+	int **energy;
+	vector<int> pic;
+	//vector< vector<int> > energy;
 	vector< vector<int> > totalEnergy;
 public:
 	SeamCarving();
@@ -24,23 +24,13 @@ public:
 	void setHeaderInfo(string typ, string comnt, string maxNum);
 	void setDimensions(string numRow, string numCol);
 	void outputPic(); //displays the picture in command line for testing
-	void outputEnergy(); //displays the energy in command line for testing
-	void outputTotalEnergy(); //displays the totalEnergy for testing
 	void buildEnergy(); //initializes energy vector
-	void buildTotalEnergy(); //initializes total energy vector
-	int calcEnergy(int i, int j);// calcs the energy value of a cell[i][j]
-	int calcTotalEnergy(int i, int j); //calcs the total energy of a cell[i][j]
-	void carveVert(); //Carve a vertical seam
-	int removeVert(int i, int j); //remove the vertical seam/calc the parents for removal
-	int pickMin(int a, int b, int c);
-	void carveHorz(); //Carve a horizontal seam
-	void removeHorz(); //remove the horizontal seam/calc the parents for removal
-	void outputFile(string filename); //outputs the current picture into a new file with the same filename, but with _processed
-
-	//void FreeDynamicArray(int** dArray)
-	//{
-	//	delete[] *dArray;
-	//	delete[] dArray;
-	//}
+	int calcEnergy(int x, int y); //Calcs the energy of a cell
+	void calcTotalVertEnergy(); //calcs the total energy vertically
+	void calcTotalHorzEnergy(); //calcs the total energy horizontally
+	void outputFile(string filename, int horzSeams); //outputs the current picture into a new file with the same filename, but with _processed
+	void carving(int verticalSeams, int horzSeams); //calls carveVert/carveHorz a specified number of times
+	void rebuildImageFromVertSeam();
+	void rebuildImageFromHorzSeam();
 };
 #endif
