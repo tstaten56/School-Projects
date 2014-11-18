e5ec8eeeb2d29d284c4c12beaa946feec66a6210
tstaten56 <tstaten56@gmail.com>
2014-11-18 15:07:45 -0500

    Just reading the file now
    Not calling any other functions in main()

--- SeamCarving/main.cpp
+++ SeamCarving/main.cpp
@@ -1,24 +1,24 @@
-//#include <iostream>
-//#include "SeamCarving.h"
-//using namespace std;
-//
-//int main (int argc, char *argv[])
-//{
-//	if (argc == 4)
-//	{
-//		string file;
-//		int vert, horz;
-//		file = argv[1];
-//		horz = atoi(argv[2]);
-//		vert = atoi(argv[3]);
-//		SeamCarving ex;
-//		ex.readFile(file, vert, horz);
-//	}
-//	else
-//	{
-//		cout << "Wrong input format!" << endl;
-//		return 1;
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+#include "SeamCarving.h"
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		string file;
+		int vert, horz;
+		file = argv[1];
+		vert = atoi(argv[2]);
+		horz = atoi(argv[3]);
+		SeamCarving ex;
+		ex.readFile(file, vert, horz);
+	}
+	else
+	{
+		cout << "Wrong input format!" << endl;
+		return 1;
+	}
+
+	return 0;
+}
